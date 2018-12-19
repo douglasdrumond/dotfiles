a2458f7f2d67ac1a74542f82ea36fdf2ad5f36cf
Douglas Drumond <douglas@cafelinear.com>
2018-12-19 02:32:50 -0200

    Remove VSCode

--- vscode/extensions/dart-code.dart-code-2.13.2/out/src/analysis/analysis_server_types.js
+++
@@ -1,6 +0,0 @@
-// This file was code-generated from the Analysis Server API doc
-// and should not be hand-edited!
-// https://github.com/Dart-Code/analysis-server-typescript-generator
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=analysis_server_types.js.map
--- vscode/extensions/dart-code.dart-code-2.13.2/out/src/debug/flutter_test_debug_entry.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const vscode_debugadapter_1 = require("vscode-debugadapter");
-const flutter_test_debug_impl_1 = require("./flutter_test_debug_impl");
-vscode_debugadapter_1.DebugSession.run(flutter_test_debug_impl_1.FlutterTestDebugSession);
-//# sourceMappingURL=flutter_test_debug_entry.js.map
